2701330f8380a59d53f19b7e6a21b2b5b70745ed
Daniel Lessa <daniellsf@id.uff.br>
2023-12-22 20:13:26 -0300

    Ajustando ngrok

--- react/src/util/constants.ts
+++ react/src/util/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://fdbb-2804-1b3-6d00-b7c3-6002-7bcc-b53b-667d.ngrok-free.app";
+export const BASE_URL = "https://ef7f-2804-1b3-6d00-b7c3-6002-7bcc-b53b-667d.ngrok-free.app";
 
 export const URL_PRODUTOS = "/produtos";
 export const URL_CATEGORIAS = "/categorias";
